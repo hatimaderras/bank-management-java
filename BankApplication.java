1f326afcbc39a29f0d3c767d4e2828220b52429f
hatimaderras <hatem.aderras.com>
2024-12-29 09:26:57 +0000

    menu of client

--- BankApplication.java
+++ BankApplication.java
@@ -34,3 +34,34 @@ public class BankApplication{
             }
         }
     }
+    public static void MenuClient(){
+        int choice=1;
+
+        while(choice!=0){
+            System.out.println("\n ----Menu Client-----");
+            System.out.println("1. Add Client ");
+            System.out.println("2. Display Client ");
+            System.out.println("3. Update  Client ");
+            System.out.println("4. Delete Client ");
+            System.out.println("0. Exit");
+            System.out.print("Enter your choice : ");
+            choice=scanner.nextInt();
+            scanner.nextLine();
+            switch(choice){
+                case 1:Client.addClient();
+                    break;
+                case 2:Client.displayClient();
+                    break;
+                case 3:Client.updateClient();
+                    break;
+                case 4:Client.deleteClient();
+                    break;
+                case 0:
+                    System.out.println("Goodbye!");
+                    return;
+                default:
+                    System.out.println("Invalid choice");
+
+            }
+        }
+    }}
