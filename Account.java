2895f438e290ed2782be19b67a45c37c0b93de18
hatimaderras <hatem.aderras.com>
2024-12-29 09:30:02 +0000

    menu for account

--- Account.java
+++ Account.java
@@ -46,4 +46,28 @@ public class Account {
     public void setClients(Client clients) {
         this.clients = clients;
     }
-    
+    public static void MenuAccount() {
+
+        int choice = 1;
+        while (choice != 0) {
+            System.out.println("------ Menu Account ------");
+            System.out.println("1- Menu of Current Account");
+            System.out.println("2- Menu of Saving Account");
+            System.out.println(" Enter your choice :");
+            choice = scanner.nextInt();
+            scanner.nextLine();
+            CurrentAccount currentAccount = new CurrentAccount();
+            SavingAccount savingAccount=new SavingAccount();
+            switch (choice) {
+                case 1:
+                    currentAccount.MenuCurrentAccount();
+                    break;
+                case 2:
+                    savingAccount.MenuSavingAccount();
+                    break;
+                default:
+                    System.out.println("Invalid choice");
+            }
+        }
+    }
+}
