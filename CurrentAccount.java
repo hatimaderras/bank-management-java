d89409c01b3c4eabe5c071ca918cece789356b8c
hatimaderras <hatem.aderras.com>
2024-12-29 09:39:07 +0000

    getters and setter for current account

--- CurrentAccount.java
+++ CurrentAccount.java
@@ -1,2 +1,32 @@
-public class CurrentAccount {
-}
+import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class CurrentAccount extends Account {
+    static int  bankCharges = 100;
+    private Client clients;
+    static Scanner scanner = new Scanner(System.in);
+    static ArrayList<CurrentAccount> currentAccount = new ArrayList<>();
+
+    public CurrentAccount(long accountNumber, double initialBalance, LocalDate creationDate, Client clients) {
+        super(accountNumber, initialBalance, creationDate, clients);
+        this.clients = clients;
+    }
+
+    public CurrentAccount(Client clients) {
+        this.clients = clients;
+    }
+
+    public CurrentAccount() {
+        super();
+    }
+
+    public int  getBankCharges() {
+        return bankCharges;
+    }
+
+    public void setBankCharges(int bankCharges) {
+        this.bankCharges = bankCharges;
+    }
+    
